b85098501b6db610743a9fa90229df7bf4b83a6b
Amresh <85226760+Amresh9@users.noreply.github.com>
2022-06-05 09:53:34 +0530

    Print F n C pattern

---
+++ 3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{ // 1.Print "F"
+
+    printf("\n");
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("\n\n");
+    // 2.Print Pattern "C"
+    printf("\n");
+    printf("  #####\n");
+    printf("##     ##\n");
+    printf("##\n");
+    printf("##\n");
+    printf("##\n");
+    printf("##\n");
+    printf(" ##    ##\n");
+    printf("  #####\n");
+
+    return 0;
+}
